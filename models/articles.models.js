b4bee89f8ed72141200329671a58dc6eea9c9f5f
bob <robert.matthew.brooks@gmail.com>
2023-06-30 10:20:19 +0100

    add comment_count to /api/articles/:article_id

--- models/articles.models.js
+++ models/articles.models.js
@@ -5,8 +5,20 @@ function getArticle(article_id) {
     return util.validateParams({ article_id })
     .then(() => {
         const queryString = `
-            SELECT * FROM articles
-            WHERE article_id = $1;
+            SELECT articles.article_id,
+                articles.title,
+                articles.topic,
+                articles.author,
+                articles.body,
+                articles.created_at,
+                articles.votes,
+                articles.article_img_url,
+                COUNT(*)::int AS comment_count
+            FROM articles
+            LEFT OUTER JOIN comments
+            ON articles.article_id = comments.article_id
+            WHERE articles.article_id = $1
+            GROUP BY articles.article_id;
         `;
 
         return db.query(queryString, [article_id]);
