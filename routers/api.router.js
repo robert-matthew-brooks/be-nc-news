a5f5cb0b2bbca8c2c86541206ce3a15784d4f2be
bob <robert.matthew.brooks@gmail.com>
2023-06-30 11:38:32 +0100

    add express routing from app.js

---
+++ routers/api.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const apiController = require('../controllers/api.controller.js');
+const topicsRouter = require('./topics.router.js');
+const articlesRouter = require('./articles.router.js');
+const commentsRouter = require('./comments.router.js');
+const usersRouter = require('./users.router.js');
+
+const apiRouter = express.Router();
+
+apiRouter.get('/', apiController.getEndpointDetails);
+
+apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/comments', commentsRouter);
+apiRouter.use('/users', usersRouter);
+
+module.exports = apiRouter;
