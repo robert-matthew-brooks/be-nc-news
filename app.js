71bc40ca1fa8b971349d27219f111f3b79926268
bob <robert.matthew.brooks@gmail.com>
2023-07-17 09:43:03 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const apiRouter = require('./routers/api-router.js');
 const errorHandlers = require('./error-handlers/error-handlers.js');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
